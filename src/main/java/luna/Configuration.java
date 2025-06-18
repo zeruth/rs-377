f54e74dd90ad00c01ba99fd53674136bd1c5fe00
Tyler Bochard <zeruth09@gmail.com>
2025-06-18 09:40:44 -0400

    init
    contains the bare minimum edits to enable connecting to luna 377 servers

---
+++ src/main/java/luna/Configuration.java
@@ -0,0 +1,13 @@
+package luna;
+
+public class Configuration {
+    public static String IP_ADDRESS = "http://127.0.0.1";
+    public static int PORT = 43594;
+    public static String CODEBASE = IP_ADDRESS + ":" + PORT;
+    public static String CACHE_DIR = "./data/cache/";
+    public static boolean CONNECT_UPDATE_SERVER = false;
+    public static boolean JAGGRAB_ENABLED = false;
+    public static boolean RSA_ENABLED = true;
+    public static boolean FIX_MOUSE_BUTTONS = true;
+    public static boolean LUNA_WALKING_BUFFER_FIX = true;
+}
